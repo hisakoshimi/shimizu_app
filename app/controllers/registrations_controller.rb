4f836475ec7daaee77abd4b580516dd9056367bf
EC2 Default User <ec2-user@ip-172-31-6-149.us-east-2.compute.internal>
2018-05-12 01:21:55 +0000

    Finish user edit action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,8 @@ end
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
+      
+      def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+      end
 end
